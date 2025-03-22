5a101a118ccb46e0178ac3d899fb2079dd59e41e
Allef Ruan <allefruan94@gmail.com>
2025-03-22 13:05:30 -0300

    atividadeTipoDados

---
+++ aula00/aula01/Questao01.js
@@ -0,0 +1,10 @@
+let n1 = 5;
+let n2 = 4;
+let n3 = 3;
+let n4 = 2;
+let n5 = 1; 
+let texto = "Aprendi";
+let texto2 = "JavaScript";
+
+console.log(n1, ",", n2, ",", n3,",", n4, ",", n5, "...", texto, texto2 );
+
